75f83a2cf48059109aa36c8b9a7e4f0180e4411a
cflannery <cmflannery@ucsd.edu>
2016-10-01 23:35:52 -0700

    lots of changes

--- pydev/ReadSerial.py
+++ pydev/ReadSerial.py
@@ -1,3 +1,4 @@
 import serial
 
-ser = serial.Serial
+# ser = serial.Serial('/dev/ttyS0', 9600)
+ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=2000, parity=serial.PARITY_EVEN, rtscts=1)
